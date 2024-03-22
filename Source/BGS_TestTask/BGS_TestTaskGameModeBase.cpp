1ac4cdc9c8da4d35bee3c5b74f54f4990ffb67bd
NicolasDaniele <nicolasdaniele@ravegan.com>
2024-03-21 22:00:26 -0300

    Replaced the default game mode for a custom one

--- Source/BGS_TestTask/BGS_TestTaskGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "BGS_TestTaskGameModeBase.h"
-
---
+++ Source/BGS_TestTask/Public/SkateboardGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkateboardGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BGS_TESTTASK_API ASkateboardGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
