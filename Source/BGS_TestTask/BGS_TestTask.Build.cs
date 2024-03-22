6906834b6d436d1fe61c1fa09581a225c8e1077d
NicolasDaniele <nicolasdaniele@ravegan.com>
2024-03-21 22:01:42 -0300

    Added the EnhancedInput module

--- Source/BGS_TestTask/BGS_TestTask.Build.cs
+++ Source/BGS_TestTask/BGS_TestTask.Build.cs
@@ -8,7 +8,7 @@ public class BGS_TestTask : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
